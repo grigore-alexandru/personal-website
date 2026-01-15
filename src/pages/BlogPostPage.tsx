7e1cdffc851ddd73f8233bdcad07a536efc61c1d
grigore-alexandru <silviualexandrugrigore@gmail.com>
2026-01-15 19:16:45 +0200

    Updated BlogPostPage.tsx

--- src/pages/BlogPostPage.tsx
+++ src/pages/BlogPostPage.tsx
@@ -152,7 +152,7 @@ const BlogPostPage: React.FC = () => {
       )}
 
       {/* Content Container */}
-      <div className="max-w-3xl mx-auto px-3 sm:px-4 lg:px-6 py-12 md:py-16">
+      <div className="max-w-3xl mx-auto px-3 sm:px-4 lg:px-6 pt-24 pb-12 md:pt-32 md:pb-16">
         {/* Post Title */}
         <h1
           className="text-black font-bold mb-8 text-center"
