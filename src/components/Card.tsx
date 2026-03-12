8ec768f4bb8d88369e78bdac36b2fb256f4dcb8c
grigore-alexandru <silviualexandrugrigore@gmail.com>
2026-03-12 15:08:41 +0200

    Updated Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -23,7 +23,7 @@ const Card: React.FC<CardProps> = ({ project }) => {
         <ProgressiveImage
           src={project.hero_image_thumbnail}
           alt={project.title}
-          className="object-cover saturate-[0.2] scale-100 group-hover:saturate-100 group-hover:scale-[1.015] transition-all duration-1000 ease-smooth"
+          className="object-cover saturate-[0.2] scale-100 group-hover:saturate-100 group-hover:scale-[1.015] transition-all duration-900 ease-smooth"
         />
       </div>
 
