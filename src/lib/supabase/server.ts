25cc911c3783da6bd57de0727c38d8972a9ac934
grigore-alexandru <silviualexandrugrigore@gmail.com>
2026-05-17 23:56:46 +0300

    Updated server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
 import type { Database } from '../../types/database';
 
 export function createServerSupabaseClient() {
-  const cookieStore = cookies();
+  const cookieStore = cookies()
   
 
   return createServerClient<Database>(
